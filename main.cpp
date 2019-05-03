dd91313d4553b0e21af8e9fa2cf6cdc7c872c060
filler36 <47821378+filler36@users.noreply.github.com>
2019-05-03 12:11:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,13 @@ int main() {
     std::cout << "Hello World\n";
     return 0;
 }
+
+void check_pass (string password)
+{
+    string valid_pass = "qwerty123";
+    if (password == valid_pass) {
+        cout << "Доступ разрешен." << endl;
+    } else {
+        cout << "Неверный пароль!" << endl;
+    }
+}
